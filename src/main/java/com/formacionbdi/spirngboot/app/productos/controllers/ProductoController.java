665a4860b659ac66947b046e3520aba5d5d56102
amarinos <amarinos@everis.com>
2021-11-03 23:06:06 -0500

    11. Creando el controlador rest productos

---
+++ src/main/java/com/formacionbdi/spirngboot/app/productos/controllers/ProductoController.java
@@ -0,0 +1,28 @@
+package com.formacionbdi.spirngboot.app.productos.controllers;
+
+import com.formacionbdi.spirngboot.app.productos.models.entity.Producto;
+import com.formacionbdi.spirngboot.app.productos.models.service.IProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductoController {
+
+    @Autowired
+    private IProductoService productoService;
+
+    @GetMapping("/listar")
+    public List<Producto> listar(){
+        return productoService.findAll();
+    }
+
+    @GetMapping("/listar/{id}")
+    public Producto detalle(@PathVariable Long id){
+        return productoService.findById(id);
+    }
+
+}
